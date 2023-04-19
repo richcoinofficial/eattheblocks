42c3213e54401e86e8fd51db4f47fcebd86303ca
richcoinofficial <130983941+richcoinofficial@users.noreply.github.com>
2023-04-19 17:19:07 +0530

    Update Token.sol

--- screencast/308-create-bep20-token-bsc/Token.sol
+++ screencast/308-create-bep20-token-bsc/Token.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.2;
 contract Token {
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
-    uint public totalSupply = 10000 * 10 ** 18;
-    string public name = "My Token";
-    string public symbol = "TKN";
+    uint public totalSupply = 10000000000 * 10 ** 18;
+    string public name = "RICH COIN";
+    string public symbol = "RC";
     uint public decimals = 18;
     
     event Transfer(address indexed from, address indexed to, uint value);
